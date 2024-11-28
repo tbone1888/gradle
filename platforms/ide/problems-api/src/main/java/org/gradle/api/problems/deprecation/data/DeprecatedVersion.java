9c06e9dd7a421816262f754b7cad5bc1786a9d2f
Bálint Hegyi <bhegyi@gradle.com>
2024-11-28 09:08:01 +0000

    Move class

--- platforms/ide/problems-api/src/main/java/org/gradle/api/problems/deprecation/data/DeprecatedVersion.java
+++ platforms/ide/problems-api/src/main/java/org/gradle/api/problems/deprecation/data/DeprecatedVersion.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.api.problems.deprecation.version;
+package org.gradle.api.problems.deprecation.data;
 
 import org.gradle.api.Incubating;
 
